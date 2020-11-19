2dd9f504f8c4fda1338279712e30b935040c4b2f
Alexandre Dosseto <leonedimars@gmail.com>
2020-11-19 21:34:53 +0100

    Spec ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "apple")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "salt")
+Ingredient.create(name: "pepper")
+Ingredient.create(name: "jack daniel's")
+Ingredient.create(name: "orange")
+Ingredient.create(name: "chery")
+Ingredient.create(name: "coca-cola")
+Ingredient.create(name: "mentos")
